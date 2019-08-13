ecf2437d82c7c04287a89e69d6c19101543666be
chengyq116 <chengyq116@163.com>
2019-08-13 11:06:00 +0800

    cfgs_FDDB_mobilenet_v1.py -->> cfgs.py

---
+++ yongqiang.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import division
+
+
+import numpy as np
+import tensorflow as tf
+
